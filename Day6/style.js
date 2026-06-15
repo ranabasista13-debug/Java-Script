cfc398a54055dbfa47a19a24bfbcfb42de538450
Bashista Rana <ranabasista13@gmail.com>
2026-06-15 10:55:24 +0545

    Learning Return type in function in Day6

---
+++ Day6/style.js
@@ -0,0 +1,57 @@
+// function greet(){
+//     let name = prompt("Enter your name: ");
+//     return name;
+// }
+// greet();
+// console.log(greet());
+
+
+
+
+//Create a function thst adds two numbers and return the sum 
+// function add(a, b){
+//     return a+b
+// }
+// let sum = add(5, 10)
+// console.log("The sum of 5 and 10 is: " + sum);
+
+
+
+//Checking age validity 
+// function isValidAge(age) {
+//     if (age >= 18){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log("Is 20 is valid age: " +true);
+// console.log("Is 15 is valid age: "+false);
+
+
+
+//Odd even check using return type 
+// function isEven (num){
+//     return num % 2 === 0;
+// }
+// console.log("Is 7 even?", isEven(7));
+// console.log("Is 10 is even?", isEven(10));
+
+
+
+
+//Find the greater number between two numbers using return type 
+// function isgreater(num1, num2){
+//     if(num1 > num2){
+//         return num1;
+//     }
+//     return num2;
+// }
+
+// console.log("Greater Between 55 and 88 is: ", isgreater(55, 88))
+
+
+
+
+
+//Create a function named calculateArea that takes the radius of a circle and find area 
